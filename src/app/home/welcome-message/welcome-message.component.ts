8f6770fc946d4b1d282cd44ec52c81b4554c051b
Franzi <franziska.lode@de.ibm.com>
2017-06-26 15:28:30 +0200

    Updated the modal at the landing page.

--- src/app/home/welcome-message/welcome-message.component.ts
+++ src/app/home/welcome-message/welcome-message.component.ts
@@ -30,4 +30,19 @@ export class WelcomeMessageComponent implements OnInit {
     // this.authService.signupUser(email, password);
   }
 
+  openTab(evt, sign_tab) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(' active', '');
+    }
+    document.getElementById(sign_tab).style.display = 'block';
+    evt.currentTarget.className += ' active';
+  }
+
+
 }
