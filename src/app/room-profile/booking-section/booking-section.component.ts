862c609aa243d29ed21a603f470670017146850f
Franzi <franziska.lode@de.ibm.com>
2017-07-14 01:06:14 +0200

    Added the "sofort buchen" pop-up.

--- src/app/room-profile/booking-section/booking-section.component.ts
+++ src/app/room-profile/booking-section/booking-section.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ModalService } from '../../shared/services/modal/modal.service';
+
 @Component({
+  moduleId: module.id.toString(),
   selector: 'app-booking-section',
   templateUrl: './booking-section.component.html',
   styles: []
 })
 export class BookingSectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit() {
+  }
+
+  // Öffnen des Pop-ups
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  // Schließen des Pop-ups
+  closeModal(id: string) {
+    this.modalService.close(id);
   }
 
 }
