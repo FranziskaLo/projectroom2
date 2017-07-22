127b0fad4fe90968c0c45f9fbcacd6b8d6046d67
Franzi <franziska.lode@de.ibm.com>
2017-07-22 19:11:03 +0200

    Deleted unnecessary code.

--- src/app/shared/services/rooms/room-rotate/room-rotate.component.ts
+++ src/app/shared/services/rooms/room-rotate/room-rotate.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
-import { NewRoomsComponent } from '../../../../home/landingpage-diashow/new-rooms/new-rooms.component';
 import { RoomBackendService } from '../room-backend/roomBackend.service';
 
 import { RoomService } from '../room-backend/room.service';
@@ -19,14 +18,7 @@ export class RoomRotateComponent implements OnInit {
 
   constructor(http: Http, private roomBackendService: RoomBackendService, private roomService: RoomService) { }
 
-  // public getRooms = (data) => {
-  //   this.rooms = data.standardRooms;
-  // }
-
   ngOnInit() {
-    // this.roomBackendService.getRooms().subscribe(
-    //  this.getRooms
-    // );
     this.getRoomsFromAPI();
   }
 
