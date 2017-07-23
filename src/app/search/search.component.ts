30f708996126f5a7a53cf238d1d81f53962bedff
Franzi <franziska.lode@de.ibm.com>
2017-07-23 19:57:16 +0200

    Started to make the search cmp dynamic.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { RoomService } from '../shared/services/rooms/room.service';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,11 +10,13 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
   citys: { city: string };
+  private rooms: any[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private roomService: RoomService) { }
 
   ngOnInit() {
     const city = this.route.snapshot.queryParams['city'];
     this.citys = { city };
+    this.rooms = this.roomService.getRooms();
   }
 }
