e6fb8a6cddaabfb079ba2002eae28e4c2e52e23d
Franzi <franziska.lode@de.ibm.com>
2017-07-23 17:41:50 +0200

    Query params in the URL are working.

--- src/app/shared/home-searchbar/home-searchbar.component.ts
+++ src/app/shared/home-searchbar/home-searchbar.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home-searchbar',
   templateUrl: './home-searchbar.component.html'
 })
 export class HomeSearchbarComponent implements OnInit {
-
+  searchForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.searchForm = new FormGroup({
+      'search': new FormControl('Search for a city...')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.searchForm);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
@@ -25,7 +26,8 @@ import { HomeSearchbarComponent } from './home-searchbar/home-searchbar.componen
   imports: [
     AppRoutingModule,
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
