90f7b01fee5245794ff13054964529239059df02
Franzi <franziska.lode@de.ibm.com>
2017-07-26 20:16:47 +0200

    Re-structured the code again.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,27 +7,26 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { HomeSearchbarComponent } from '../shared/home-searchbar/home-searchbar.component';
-import { LandingpageDiashowComponent } from './landingpage-diashow/landingpage-diashow.component';
-import { RecommendedRoomsComponent } from './landingpage-diashow/recommended-rooms/recommended-rooms.component';
-import { NewRoomsComponent } from './landingpage-diashow/new-rooms/new-rooms.component';
-import { RandomRoomsComponent } from './landingpage-diashow/random-rooms/random-rooms.component';
-import { RoomRotateComponent } from './landingpage-diashow/room-rotate/room-rotate.component';
+import { RoomCategoriesComponent } from './room-categories/room-categories.component';
+import { RecommendedRoomsComponent } from './room-categories/recommended-rooms/recommended-rooms.component';
+import { NewRoomsComponent } from './room-categories/new-rooms/new-rooms.component';
+import { RandomRoomsComponent } from './room-categories/random-rooms/random-rooms.component';
+import { RoomRotateComponent } from './room-categories/room-rotate/room-rotate.component';
 import { WelcomeMessageComponent } from './welcome-message/welcome-message.component';
 import { MemberCommentsComponent } from './member-comments/member-comments.component';
-
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HomeHeaderComponent,
-    LandingpageDiashowComponent,
     RecommendedRoomsComponent,
     NewRoomsComponent,
     RandomRoomsComponent,
     WelcomeMessageComponent,
     MemberCommentsComponent,
-    RoomRotateComponent
+    RoomRotateComponent,
+    RoomCategoriesComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/home/room-categories/room-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-categories',
+  templateUrl: './room-categories.component.html',
+  styles: []
+})
+export class RoomCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
